75dc1815fb24bc94e947beb7754ed296b821116a
David Lee <ing.jdplee@gmail.com>
2023-07-22 15:10:30 -0400

    Folder Setup - Step

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+const DashboardPage = () => {
+  return <div> Dashboard Page (Protected)</div>;
+};
+
+export default DashboardPage;
