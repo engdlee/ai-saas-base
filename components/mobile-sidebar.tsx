e723ad1acb572e15acd4caf7d6232da049ea0e4f
David Lee <ing.jdplee@gmail.com>
2023-07-22 17:13:49 -0400

    Dashboard - Step

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { Menu } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
@@ -7,6 +8,15 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import Sidebar from '@/components/sidebar';
 
 export const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Sheet>
       <SheetTrigger>
